74295817467b11ef552b982eff77813d63535415
antomo <tomoan0121@gmail.com>
2022-06-19 13:12:08 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -30,6 +30,21 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 bool consume(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
     return false;
@@ -98,7 +113,8 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  token = tokenize(argv[1]);
+  user_input = argv[1];
+  token = tokenize(user_input);
   
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
